cee557ff36420b0117bf044e1f38fefff672be03
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2019-12-10 08:31:36 +0100

    Refactor and use consumer

--- todo_flutter/lib/widgets/tasks_list.dart
+++ todo_flutter/lib/widgets/tasks_list.dart
@@ -3,29 +3,26 @@ import 'package:provider/provider.dart';
 import 'package:todo_flutter/models/task_data.dart';
 import 'package:todo_flutter/widgets/task_tile.dart';
 
-class TasksList extends StatefulWidget {
-  TasksList();
-
-  @override
-  _TasksListState createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-            taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-            //Callback
-            checkboxCallback: (checkboxState) {
-              setState(() {
-                Provider.of<TaskData>(context).tasks[index].toggleDone();
-              });
-            });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+                taskTitle: taskData.tasks[index].name,
+                isChecked: taskData.tasks[index].isDone,
+                //Callback
+                checkboxCallback: (checkboxState) {
+                  // setState(() {
+                  //   Provider.of<TaskData>(context).tasks[index].toggleDone();
+                  // });
+                });
+          },
+          itemCount: taskData.taskCount,
+        );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
