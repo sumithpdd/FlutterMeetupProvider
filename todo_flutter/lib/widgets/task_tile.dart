b151a0b7a88bb122663b1d9e32a3eef3451e702b
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2019-12-10 06:54:03 +0100

    Task Checkbox with state and callback

--- todo_flutter/lib/widgets/task_tile.dart
+++ todo_flutter/lib/widgets/task_tile.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
+  //Callback
+  void checkBoxCallBack(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool value) {},
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null,
+            color: Colors.black),
       ),
+      trailing: new TaskCheckBox(
+          checkboxState: isChecked, 
+          toggleCheckBoxState: checkBoxCallBack),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckBoxState;
+  TaskCheckBox({this.checkboxState, this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Theme.of(context).accentColor,
+      value: checkboxState,
+      onChanged: (bool newValue) {},
     );
   }
 }
