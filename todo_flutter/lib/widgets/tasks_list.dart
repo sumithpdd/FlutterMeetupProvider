61984980336f7b4332f4b65d0c37596a480b6058
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2019-12-10 11:37:30 +0100

    Checkbox using provider. still stateless widgets

--- todo_flutter/lib/widgets/tasks_list.dart
+++ todo_flutter/lib/widgets/tasks_list.dart
@@ -10,14 +10,13 @@ class TasksList extends StatelessWidget {
       builder: (context, taskData, child) {
        return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
             return TaskTile(
-                taskTitle: taskData.tasks[index].name,
-                isChecked: taskData.tasks[index].isDone,
+                taskTitle: task.name,
+                isChecked: task.isDone,
                 //Callback
                 checkboxCallback: (checkboxState) {
-                  // setState(() {
-                  //   Provider.of<TaskData>(context).tasks[index].toggleDone();
-                  // });
+                 taskData.updateTask(task);
                 });
           },
           itemCount: taskData.taskCount,
